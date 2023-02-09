3cf078e8345b124a18f068c6985773cec4d7fcae
nandur9gmail <nandur9gmail@ip-172-31-27-176.ec2.internal>
2023-02-09 15:38:33 +0000

    OverriddingDemo

---
+++ Repo/src/OverRide.java
@@ -0,0 +1,12 @@
+
+public class OverRide {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	   OverriddingB obj = new OverriddingB(10,20,30);
+	   obj.show();
+
+	}
+
+}
