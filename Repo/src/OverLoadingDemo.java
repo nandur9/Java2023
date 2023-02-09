bef6ac0b3ed0959f40d1788e44e63d886deee5c5
nandur9gmail <nandur9gmail@ip-172-31-27-176.ec2.internal>
2023-02-09 14:11:33 +0000

    OverLoadingDemo

---
+++ Repo/src/OverLoadingDemo.java
@@ -0,0 +1,17 @@
+
+public class OverLoadingDemo {
+	
+	void hello()
+	{
+		System.out.println("hello with no perameters");
+	}
+	void hello(int a)
+	{
+		System.out.println("hello with 1 perameters of typs integer");
+	}
+	void hello(int a,int b)
+	{
+		System.out.println("hello with 2 perameters of typs integer:"+a+","+b);
+	}
+
+}
