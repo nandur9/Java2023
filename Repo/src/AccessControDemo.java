ff7f5595560a7a5935dc07734c67e546c4a8a02f
nandur9gmail <nandur9gmail@ip-172-31-27-176.ec2.internal>
2023-02-09 01:39:32 +0000

    AccessControDemo

---
+++ Repo/src/AccessControDemo.java
@@ -0,0 +1,18 @@
+
+public class AccessControDemo {
+	
+		int a ;
+		public int b;
+		private int c;
+		public String getC;
+		void setC(int i)
+		{
+			c = i;
+		}
+		
+		int getc()
+		{
+			return c;
+		}
+		
+	}
