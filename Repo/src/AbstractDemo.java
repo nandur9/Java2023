6331155a05a828a73bf56ff8d4cd4f044b816138
nandur9gmail <nandur9gmail@ip-172-31-27-176.ec2.internal>
2023-02-09 16:13:13 +0000

    AbstractDemo

---
+++ Repo/src/AbstractDemo.java
@@ -0,0 +1,37 @@
+
+public class AbstractDemo {
+
+	public static void main(String[] args) 
+	{
+		AbstractB obj =new AbstractB();
+	         obj.callme();
+		     obj.callmetoo();
+		 
+
+	}
+
+}
+/*abstract class Sunstar {
+    abstract void printInfo();
+}
+ 
+class Employee extends Sunstar {
+    void printInfo() {
+        String name = "avinash";
+        int age = 21;
+        float salary = 222.2F;
+ 
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(salary);
+ 
+    }
+ 
+}
+ 
+class Base {
+    public static void main(String args[]) {
+        Sunstar s = new Employee();
+        s.printInfo();
+    }
+}*/
