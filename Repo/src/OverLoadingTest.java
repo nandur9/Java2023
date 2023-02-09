c98331d60bd5bc183d761ed4fd4d0f98d65712ce
nandur9gmail <nandur9gmail@ip-172-31-27-176.ec2.internal>
2023-02-09 14:12:10 +0000

    OverLoadingTest

---
+++ Repo/src/OverLoadingTest.java
@@ -0,0 +1,14 @@
+
+public class OverLoadingTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		OverLoadingDemo obj1 = new OverLoadingDemo();
+		obj1.hello();
+		obj1.hello(200);
+		obj1.hello(200,300);
+
+	}
+
+}
